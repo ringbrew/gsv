fb7d7641132ec4088f5596213f14f16381843a53
ring <ringxu666@gmail.com>
2022-06-08 16:37:51 +0800

    add http handler collector

--- service/service.go
+++ service/service.go
@@ -26,7 +26,7 @@ type Description struct {
 	Valid           bool
 	GrpcServiceDesc []grpc.ServiceDesc
 	GrpcGateway     []GatewayRegister
-	HttpRoute       []HttpRoute
+	HttpRoute       HttpRouteCollector
 }
 
 type GatewayRegister func(ctx context.Context, mux *runtime.ServeMux, conn *grpc.ClientConn) error
@@ -35,4 +35,35 @@ type HttpRoute struct {
 	Path    string
 	Method  string
 	Handler http.HandlerFunc
+}
+
+type HttpRouteCollector []HttpRoute
+
+func (c *HttpRouteCollector) append(method string, path string, handler http.HandlerFunc) {
+	r := HttpRoute{
+		Method:  method,
+		Path:    path,
+		Handler: handler,
+	}
+	*c = append(*c, r)
+}
+
+func (c *HttpRouteCollector) MapMethods(method string, path string, handler http.HandlerFunc) {
+	c.append(method, path, handler)
+}
+
+func (c *HttpRouteCollector) Get(path string, handler http.HandlerFunc) {
+	c.MapMethods(http.MethodGet, path, handler)
+}
+
+func (c *HttpRouteCollector) Post(path string, handler http.HandlerFunc) {
+	c.MapMethods(http.MethodPost, path, handler)
+}
+
+func (c *HttpRouteCollector) Put(path string, handler http.HandlerFunc) {
+	c.MapMethods(http.MethodPut, path, handler)
+}
+
+func (c *HttpRouteCollector) Delete(path string, handler http.HandlerFunc) {
+	c.MapMethods(http.MethodDelete, path, handler)
 }
