b256d0d8b893c384236720b5b98a24a04e660379
ringbrew <ringxu666@163.com>
2022-11-28 15:15:55 +0800

    feat: level string

--- logger/logger.go
+++ logger/logger.go
@@ -31,6 +31,25 @@ const (
 	LevelOff
 )
 
+func (l Level) String() string {
+	switch l {
+	case LevelDebug:
+		return "DEBUG"
+	case LevelInfo:
+		return "INFO"
+	case LevelWarn:
+		return "WARN"
+	case LevelError:
+		return "ERROR"
+	case LevelFatal:
+		return "FATAL"
+	case LevelOff:
+		return "OFF"
+	default:
+		return ""
+	}
+}
+
 type LogEntry struct {
 	TraceId  string
 	SpanId   string
