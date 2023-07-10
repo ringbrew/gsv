6caee91b879ee0e303617ae3a5dfa91819ca1ffe
ringbrew <ringxu666@163.com>
2023-07-10 18:01:04 +0800

    opt: local option for discovery schema

--- discovery/scheme.go
+++ discovery/scheme.go
@@ -2,6 +2,12 @@ package discovery
 
 import "fmt"
 
-func Scheme(service string) string {
+func Scheme(service string, localOpt ...map[string]string) string {
+	if len(localOpt) > 0 {
+		if val, exist := localOpt[0][service]; exist {
+			return val
+		}
+	}
+
 	return fmt.Sprintf("%s:///%s", SchemeName, service)
 }
